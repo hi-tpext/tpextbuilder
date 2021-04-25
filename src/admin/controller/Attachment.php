fba3029e6e57fa0fabc3caa11676d2577d569755
ichynul <ichynul@163.com>
2021-04-25 23:09:37 +0800

    update

--- src/admin/controller/Attachment.php
+++ src/admin/controller/Attachment.php
@@ -37,7 +37,7 @@ class Attachment extends Controller
 
     protected function filterWhere()
     {
-        $searchData = request()->post();
+        $searchData = request()->get();
 
         $where = [];
 
