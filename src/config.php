cd6110b17575a093db3ca0a9364b17156f7362eb
ichynul <ichynul@163.com>
2022-04-06 15:36:04 +0800

    高德地图[安全密钥]支持

--- src/config.php
+++ src/config.php
@@ -23,7 +23,7 @@ return [
     'table_empty_text' => '<div class="text-center"><img src="/assets/tpextbuilder/images/empty.png" /><p>暂无相关数据~</p></div>',
     //
     '__hr__' => '地图api，按需配置',
-    'amap_js_key' => '//webapi.amap.com/maps?v=1.4.15&key=您申请的key值',
+    'amap_js_key' => '//webapi.amap.com/maps?v=1.4.15&key=您申请的key&jscode=你的jscode',
     'baidu_map_js_key' => '//api.map.baidu.com/api?v=3.0&ak=您的密钥',
     'google_map_js_key' => '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places&key=您申请的key值',
     'tcent_map_js_key' => '//map.qq.com/api/js?v=2.exp&libraries=place&key=您申请的key值',
@@ -39,7 +39,7 @@ return [
         'file_by_date' => ['type' => 'radio', 'label' => '文件目录归档', 'options' => [1 => 'Ym(年月)', 2 => 'Ymd(年月日)', 3 => 'Y/m(年/月)', 4 => 'Y/md(年/月日)', 5 => 'Ym/d(年月/日)', 6 => 'Y/m/d(年/月/日)']],
         'storage_driver' => ['type' => 'select', 'label' => '存储驱动', 'col_size' => 6, 'size' => [4, 8], 'options' => Module::getInstance()->getStorageDrivers()],
         'table_empty_text' => ['type' => 'textarea', 'label' => '列表无数据显示', 'size' => [2, 10], 'help' => '支持html'],
-        'amap_js_key' => ['type' => 'text', 'label' => '高德地图js'],
+        'amap_js_key' => ['type' => 'text', 'label' => '高德地图js', 'help' => '[高德]自2021年12月02日升级之后所申请的`key`必须配备安全密钥`jscode`一起使用。两个参数按`&key=你的key&jscode=你的jscode'],
         'baidu_map_js_key' => ['type' => 'text', 'label' => '百度地图js'],
         'google_map_js_key' => ['type' => 'text', 'label' => 'google地图js'],
         'tcent_map_js_key' => ['type' => 'text', 'label' => '腾讯地图js'],
