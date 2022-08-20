36b7ad786dae204af1fb17ed00afafa714fd2747
ichynul <ichynul@163.com>
2022-08-20 18:24:21 +0800

    适配webman

---
+++ src/webman/Init.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace tpext\builder\webman;
+
+use Webman\Http\Request;
+use Webman\Http\Response;
+use Webman\MiddlewareInterface;
+use tpext\builder\common\Builder;
+use tpext\builder\common\Module;
+
+/**
+ * for webman
+ */
+
+class Init implements MiddlewareInterface
+{
+    public function process(Request $request, callable $next): Response
+    {
+        Builder::destroyInstance();
+
+        $response = $next($request);
+
+        Module::getInstance()->setUploadUrl('');
+        Module::getInstance()->setImportUrl('');
+        Module::getInstance()->setChooseUrl('');
+        Module::getInstance()->setViewsPath('');
+
+        Builder::destroyInstance();
+
+        return $response;
+    }
+}
