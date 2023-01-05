906f038f3e9f43c520168b6a09e0798dfd6fb963
ichynul <ichynul@163.com>
2023-01-05 14:27:38 +0800

    列表页排除字段

--- src/traits/actions/HasIndex.php
+++ src/traits/actions/HasIndex.php
@@ -50,6 +50,12 @@ trait HasIndex
     protected $indexWith = [];
 
     /**
+     * 列表页排除的字段，过长的文本字段影响查询速度，而列表页又不显示此字段时可以把它排除
+     * @var array|string
+     */
+    protected $indexFieldsExcept = '';
+
+    /**
      * 允许行内编辑的字段，留空则不限制
      *
      * @var array
@@ -205,7 +211,7 @@ trait HasIndex
         //可重写此方法，比如用户点击除了create_time以外的任何个字段排序，都再加一个`create_time`倒序。
 
         // if (!strstr($sortOrder, 'create_time')) {
-        //     return $sortOrder .' create_time desc';
+        //     return $sortOrder .',create_time desc';
         // }
 
         return $sortOrder;
@@ -233,11 +239,15 @@ trait HasIndex
             $data = $this->dataModel->with($this->indexWith)
                 ->where($where)
                 ->order($sortOrder)
+                ->field(empty($this->indexFieldsExcept))
+                ->withoutField($this->indexFieldsExcept)
                 ->cursor(); //select和cursor均可，最好是返回cursor
         } else {
             $data = $this->dataModel->with($this->indexWith)
                 ->where($where)->order($sortOrder)
                 ->limit(($page - 1) * $this->pagesize, $this->pagesize)
+                ->field(empty($this->indexFieldsExcept))
+                ->withoutField($this->indexFieldsExcept)
                 ->select();
         }
 
